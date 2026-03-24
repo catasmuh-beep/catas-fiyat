fccc1371c0d45e2e992a04849b80aaddeafb5e78
catasmuh-beep <catasmuh@gmail.com>
2026-03-24 10:09:50 +0300

    Update route.js

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -16,10 +16,15 @@ export async function PATCH(request, { params }) {
       marka: norm(body.marka),
       model: norm(body.model),
       alt_model: norm(body.alt_model),
+
       alis_fiyati: Number(body.alis_fiyati || 0),
       montaj_maliyeti: Number(body.montaj_maliyeti || 0),
       puan: Number(body.puan || 0),
       fayda: Number(body.fayda || 0),
+
+      nakit_carpani: Number(body.nakit_carpani || 9),
+      kart_komisyon: Number(body.kart_komisyon || 18),
+
       aktif: !!body.aktif,
     };
 
