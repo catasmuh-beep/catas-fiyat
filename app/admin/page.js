a79e186cbe1d9442af6f520143bb43ed8ce7d443
catasmuh-beep <catasmuh@gmail.com>
2026-03-24 11:19:20 +0300

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,27 +1,5 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { getServerSupabase } from "../lib/supabase";
 import AdminClient from "./admin-client";
 
-function isAuthed() {
-  const store = cookies();
-  return store.get("catas_admin")?.value === "1";
-}
-
-export default async function AdminPage() {
-  const authed = isAuthed();
-
-  if (!authed) {
-    redirect("/admin/login");
-  }
-
-  const supabase = getServerSupabase();
-
-  const { data, error } = await supabase.from("products").select("*");
-
-  if (error) {
-    throw new Error(error.message);
-  }
-
-  return <AdminClient initialRows={data || []} />;
+export default function AdminPage() {
+  return <AdminClient />;
 }
