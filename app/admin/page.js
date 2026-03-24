ee4df1727b48f4f0a9411da6733119229687f7e6
catasmuh-beep <catasmuh@gmail.com>
2026-03-24 11:36:13 +0300

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,5 +1,14 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import AdminClient from "./admin-client";
 
-export default function AdminPage() {
+export default async function AdminPage() {
+  const cookieStore = await cookies();
+  const authed = cookieStore.get("catas_admin")?.value === "1";
+
+  if (!authed) {
+    redirect("/admin/login");
+  }
+
   return <AdminClient />;
 }
