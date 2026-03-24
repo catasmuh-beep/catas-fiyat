87372cd478c781d3716b6a07c3ae4bfe2150c4ae
catasmuh-beep <catasmuh@gmail.com>
2026-03-24 13:13:42 +0300

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -2,13 +2,20 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import AdminClient from "./admin-client";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+async function isAuthed() {
+  const store = cookies();
+  return store.get("catas_admin")?.value === "1";
+}
+
 export default async function AdminPage() {
-  const cookieStore = await cookies();
-  const authed = cookieStore.get("catas_admin")?.value === "1";
+  const authed = await isAuthed();
 
   if (!authed) {
     redirect("/admin/login");
   }
 
-  return <AdminClient />;
+  return <AdminClient initialProducts={[]} />;
 }
